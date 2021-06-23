17e0e4148052704ded982220d6413a5962410e71
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2021-06-23 10:44:08 +0700

    update code

--- src/corona/components/Countries.js
+++ src/corona/components/Countries.js
@@ -1,44 +1,48 @@
 import React from 'react';
 import { helper } from '../helpers/common';
+import CoronaContext from '../context/index';
 
-const CountriesCorona = ({ error, data }) => {
-
-  if(!helper.isEmptyObject(error) || helper.isEmptyObject(data)){
-    // co loi
-    return (
-      <h1 style={{ color: 'red' }}> {error.mess} </h1>
-    )
-  }
+const CountriesCorona = () => {
 
   return (
-    <table width="100%" border="1" cellSpacing="0" cellPadding="0">
-      <thead>
-        <tr>
-          <th>Country</th>
-          <th>Country Code</th>
-          <th>New Confirmed</th>
-          <th>Total Confirmed</th>
-          <th>New Deaths</th>
-          <th>Total Deaths</th>
-          <th>New Recovered</th>
-          <th>Total Recovered</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.Countries.map(item => (
-          <tr key={item.ID}>
-            <td>{item.Country}</td>
-            <td>{item.CountryCode}</td>
-            <td>{item.NewConfirmed}</td>
-            <td>{item.TotalConfirmed}</td>
-            <td>{item.NewDeaths}</td>
-            <td>{item.TotalDeaths}</td>
-            <td>{item.NewRecovered}</td>
-            <td>{item.TotalRecovered}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <CoronaContext.Consumer>
+      {context => {
+        if(context.loading || helper.isEmptyObject(context.virus)){
+          return <h1> Loading data countries...</h1>
+        } else {
+          return (
+          <table width="100%" border="1" cellSpacing="0" cellPadding="0">
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Country Code</th>
+                <th>New Confirmed</th>
+                <th>Total Confirmed</th>
+                <th>New Deaths</th>
+                <th>Total Deaths</th>
+                <th>New Recovered</th>
+                <th>Total Recovered</th>
+              </tr>
+            </thead>
+            <tbody>
+              {context.virus.Countries.map(item => (
+                <tr key={item.ID}>
+                  <td>{item.Country}</td>
+                  <td>{item.CountryCode}</td>
+                  <td>{item.NewConfirmed}</td>
+                  <td>{item.TotalConfirmed}</td>
+                  <td>{item.NewDeaths}</td>
+                  <td>{item.TotalDeaths}</td>
+                  <td>{item.NewRecovered}</td>
+                  <td>{item.TotalRecovered}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      }}
+    </CoronaContext.Consumer>
+    
   )
 }
 export default React.memo(CountriesCorona);
--- src/corona/components/Global.js
+++ src/corona/components/Global.js
@@ -5,20 +5,31 @@ import CoronaContext from '../context/index';
 const GlobalCorona = () => {
 
   return (
-    <section className="global__corona">
-      <div className="global__item">
-        <p> - Moi nhiem : 12122</p>
-        <p> - Tong ca nhiem : 121212</p>
-      </div>
-      <div className="global__item">
-        <p> - Moi tu vong:  21212</p>
-        <p> - Tong ca tu vong:  232323</p>
-      </div>
-      <div className="global__item">
-        <p> - Moi khoi benh:  232323</p>
-        <p> - Tong ca khoi benh:  21212</p>
-      </div>
-    </section>
+    <CoronaContext.Consumer>
+      {context => {
+        if(context.loading || helper.isEmptyObject(context.virus)){
+          return <h1> Loading data global ...</h1>
+        } else {
+          return(
+            <section className="global__corona">
+              <div className="global__item">
+                <p> - Moi nhiem : {context.virus.Global.NewConfirmed}</p>
+                <p> - Tong ca nhiem : {context.virus.Global.TotalConfirmed}</p>
+              </div>
+              <div className="global__item">
+                <p> - Moi tu vong:  {context.virus.Global.NewDeaths}</p>
+                <p> - Tong ca tu vong:  {context.virus.Global.TotalDeaths}</p>
+              </div>
+              <div className="global__item">
+                <p> - Moi khoi benh:  {context.virus.Global.NewRecovered}</p>
+                <p> - Tong ca khoi benh:  {context.virus.Global.TotalRecovered}</p>
+              </div>
+            </section>
+          )
+        }
+      }}
+    </CoronaContext.Consumer>
+    
   )
 }
 export default React.memo(GlobalCorona);
